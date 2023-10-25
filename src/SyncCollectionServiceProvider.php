50d5aa1bb0d9d1b3ba4f7aafaa024eafd8d7e24c
dayCod <alayarahman07@gmail.com>
2023-10-25 09:39:22 +0700

    Initial Commit

---
+++ src/SyncCollectionServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Rembon\SyncCollection;
+
+use Illuminate\Support\ServiceProvider;
+
+class SyncCollectionServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}

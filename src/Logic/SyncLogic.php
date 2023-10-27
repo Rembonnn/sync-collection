9a30e446ae1eff7cd64c01f4c4c4917ed567f9ec
dayCod <alayarahman07@gmail.com>
2023-10-27 16:19:52 +0700

    Update: With MultiDimensionBetween Sync Logical Func

--- src/Logic/SyncLogic.php
+++ src/Logic/SyncLogic.php
@@ -2,6 +2,7 @@
 
 namespace Rembon\SyncCollection\Logic;
 
+use Exception;
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Database\Eloquent\Model;
@@ -9,6 +10,20 @@ use LogicException;
 
 class SyncLogic
 {
+    /**
+     * Apendables data getter
+     *
+     * @var array
+     */
+    protected $appends = [];
+
+    /**
+     * Curents data getter
+     *
+     * @var array
+     */
+    protected $currents = [];
+
     /**
      * Process the Between Old and New Singular Collection With Key
      *
@@ -51,26 +66,61 @@ class SyncLogic
      *
      * @return Collection|array
      */
-    public function withManyBetween(
+    public function withMultiDimensionBetween(
         Collection|Model $old_collection,
         Collection|Model $new_collection,
         string $unique_key = "",
         array $columns_to_merge = []
     ) {
-        $merged_data = $old_collection->concat($new_collection);
-        $synced_data = $merged_data->groupBy($unique_key)->map(function ($items, $index) use ($columns_to_merge) {
-            $columns_to_merge = $this->uniqueArrayKey($columns_to_merge);
-            return $items->reduce(function ($carry, $item) use ($columns_to_merge) {
-                $merged_item = ['id' => $carry['id'] ?? $item['id'] ?? null];
-                foreach ($columns_to_merge as $column) {
-                    $merged_item[$column] = max($carry[$column] ?? 0, $item[$column]);
-                }
+        $merge_old_new_collection = $old_collection->concat($new_collection);
+        $merge_old_new_collection->each(function ($item, $key) use ($unique_key) {
+            in_array($unique_key, array_keys($item))
+                ? array_push($this->currents, $item)
+                : array_push($this->appends, $item);
+        });
 
-                return $merged_item;
-            }, []);
-        })->values();
+        $separate = $this->separateCollectionWithUnique(collect($this->currents), $unique_key);
 
-        return $synced_data;
+        return collect([
+            'currents' => $separate['currents'],
+            'appends' => collect($this->appends),
+            'olds' => $separate['removes'],
+            'updated' => $separate['updated'],
+        ]);
+    }
+
+    /**
+     * Separate Between use and unused Data Collection
+     *
+     * @param Collection|Model $collection
+     * @param string $unique_key
+     *
+     * @return Collection
+     */
+    private function separateCollectionWithUnique(Collection|Model $collection, string $unique_key)
+    {
+        $results = collect([]);
+
+        $get_duplicate = $collection->duplicates($unique_key);
+        $get_duplicate->keys()->each(function ($index_value, $key) use ($collection, $results) {
+            $results->push($collection[$index_value]);
+        });
+
+        $remove_old_with_new = $collection->filter(function ($item, $key) use ($results, $unique_key) {
+            $last_item = array();
+            foreach ($results as $key => $val) {
+                array_push($last_item, $val);
+            }
+            return !in_array($item, $last_item);
+        });
+
+        $currents = $remove_old_with_new->splice($results->count());
+
+        return collect([
+            'currents' => $currents,
+            'removes' => $remove_old_with_new,
+            'updated' => $results
+        ]);
     }
 
     /**
@@ -79,7 +129,7 @@ class SyncLogic
      * @param array $key
      * @return array
      */
-    public function uniqueArrayKey(array $key)
+    private function uniqueArrayKey(array $key)
     {
         return array_unique($key);
     }
